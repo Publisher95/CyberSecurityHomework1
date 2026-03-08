d89181bd3cb42269567a6e98c1f5bdb499fafb7e
Michael Gavina <Mikeygavina@yahoo.com>
2026-03-08 16:22:33 -0600

    finished project file

--- solve_template.py
+++ solve_template.py
@@ -7,8 +7,34 @@ def solve(ciphertext):
     with open(ciphertext, "rb") as f:
         data = f.read()
 
+    if len(data) % 16 != 0:
+        data += "00" * (16 - (len(data) % 16))
+
+
     chunks = [data[i:i+16].hex() for i in range(0,len(data), 16)]
 
-    print(chunks)
+    unique = []
+    repeat = []
 
-solve(input("Enter in the ciphertext: "))
+    for i in range(len(chunks)):
+        if (chunks[i] not in unique):
+            unique.append(chunks[i])
+        elif (chunks[i] not in repeat):
+            repeat.append(chunks[i])
+    
+    for i in range(len(repeat)):
+        if (repeat[i] in unique ):
+            unique.remove(repeat[i])
+    
+    print("Unique: ")
+    for i in range(len(unique)):
+        print(unique[i] + " ")
+
+
+    print("Repeating: ")
+    for i in range(len(repeat)):
+        print(repeat[i] + " ")
+
+#solve(input("Enter in the ciphertext: "))
+
+solve("challenge.bin")
--- student_report.md
+++ student_report.md
@@ -3,12 +3,21 @@ Identify the cryptographic misuse and recover the secret.
 
 ## Explanation
 - What is broken?
+
+
 - Why is it insecure?
+
+
 
 ## Solution
 - Attack strategy
+
+
 - Code overview
+
 
 ## Mitigation
 - How should this be fixed?
+
+
 
